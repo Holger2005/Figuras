5525f289f0b0b6f026107166053b3c95be2ac030
Holger2005 <ordonezholger43@gmail.com>
2026-09-07 16:44:54 -0500

    feat: crear clase Graficador con método para graficar figuras

---
+++ src/com/krakedev/figuras/Graficador.java
@@ -0,0 +1,12 @@
+package com.krakedev.figuras;
+
+public class Graficador {
+	
+	public void graficar(Figura figura) {
+		
+		Figura grafica = figura;
+		
+		System.out.println("Graficando " + grafica.getNombre() + " de color " + grafica.getColor());
+	}
+
+}
