ff8bdbac977666fd5493848794b26bca56bf9dd1
Holger2005 <ordonezholger43@gmail.com>
2026-09-07 16:16:54 -0500

    test: crear paquete de pruebas e implementar TestFiguras

---
+++ src/com/krakedev/test/TestFiguras.java
@@ -0,0 +1,19 @@
+package com.krakedev.test;
+
+import com.krakedev.figuras.Cuadrado;
+import com.krakedev.figuras.Figura;
+import com.krakedev.figuras.Triangulo;
+
+public class TestFiguras {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Figura figura = new Figura();
+		Cuadrado cuadrado = new Cuadrado();
+		Triangulo triangulo = new Triangulo();
+
+		System.out.println(triangulo);
+	}
+
+}
